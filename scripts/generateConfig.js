54a2576f03c3601f8b46baca8f098ca188ae4bae
ashar <ashar@ashars-MacBook-Pro.local>
2025-11-28 06:42:07 -0500

    Added config

---
+++ scripts/generateConfig.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+const data = `
+window.firebaseConfig = {
+  apiKey: "${process.env.FIREBASE_API_KEY}",
+  authDomain: "${process.env.FIREBASE_AUTH_DOMAIN}",
+  projectId: "${process.env.FIREBASE_PROJECT_ID}",
+  storageBucket: "${process.env.FIREBASE_STORAGE_BUCKET}",
+  messagingSenderId: "${process.env.FIREBASE_MESSAGING_SENDER_ID}",
+  appId: "${process.env.FIREBASE_APP_ID}"
+};
+`;
+
+fs.writeFileSync("config.js", data);
+console.log("config.js created from environment variables ✔");
